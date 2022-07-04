f2c22c67d3113d6432c8797d2e81204839354f9e
arjuns81 <95590509+arjuns81@users.noreply.github.com>
2022-07-04 14:59:02 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+
+canvas=document.getElementById("tail")
+ctx= canvas.getContext("2d")
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); 
+	img_imgTag.onload = uploadimg; 
+	img_imgTag.src = img_image;  aplhabetkey.png,Arrkey.png
+
+function uploadimg() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
+}
+function my_keydown(e)
+{  window.addEventListener ("keydown", my-keydown);
+ 	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		w
+	else{
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	}
+}
+
+
+function my_keydown (e);
+keyPressed.e.keyCode;
+console.log(keyPressed);
+function aplhabetkey()
+
+if(keyPressed>=97&&keyPressed<=122)// (keyPressed >=65 && keyPressed <=90)
+document.getElementById("d1").innerHTML=YoupressedAlphabetkey
+console.log("alphabet key");
+{
+
+}
+function numberkey()
+{
+	
+}
+function arrowkey()
+{
+}
+function specialkey()
+{
+	
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
